d8512e1d0fbc3ee8159961cdd65df1b2646ffd0e
JAYAPRASAD B <127640948+Jayaprasad78@users.noreply.github.com>
2024-01-26 22:13:14 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,7 @@
-export const host = "http://localhost:5000";
+// Update the host variable to the online backend URL
+export const host = "https://chat-app3-server.vercel.app";
+
+// Update other routes accordingly
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
